5539ef2205105af501ecfd977f53fe86484f1d6f
Alexander Chumakov <w5292c@gmail.com>
2015-03-20 08:30:06 +0200

    Updated documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file defines the framework for cached inverse matrix computation.
+## First we need to prepare the cache structure using 'makeCacheMatrix',
+## after this, 'cacheSolve' can be used in order to compute inverse of
+## the original matrix. If cache is not ready, 'cacheSolve' will compute
+## the matrix inverse and store it internally, after this, the cached
+## value of the inverse matrix will be availabe without computing
+## it second time.
+## Here is a simple example:
+## > # Define the original martix(s)
+## > originalMatrix <- matrix(c(1,2,2,1), 2, 2)
+## > # Now, prepare the cache structure (matrix)
+## > inverseCache <- makeCacheMatrix(originalMatrix)
+## > # Now, we are ready to compute the inverse of the original matrix
+## > # First, as the inverse matrix is not available, we will compute it
+## > # and store it internally in 'inverseCache' structure.
+## > finalInverse1 <- cacheSolve(inverseCache)
+## > # When we request the inverse the 2nd time, we will get
+## > # the cached value, no need to compute it again.
+## > finalInverse2 <- cacheSolve(inverseCache)
 
-## Write a short comment describing this function
+## This function prepares cache structure that will be used for
+## managing cached value of the matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         cv <- NULL
@@ -18,9 +36,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function can be used for computing inverse of the input matrix 'x'.
+## If the inverse has alreay been calculated, the cached value is returned.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
         cv <- x$getcache()
         if(!is.null(cv)) {
